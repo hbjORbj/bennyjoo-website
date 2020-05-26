1cddcde8845166c7536b0d16169105c540d974fc
hbjORbj <sldisek783@gmail.com>
2020-05-26 11:11:21 +0100

    2/3 Complete

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const showClickedCategory = (event) => {
+  const id = event.id;
+  const links = document.querySelectorAll(".work__category button");
+  for (i = 0; i < links.length; i++) {
+    if (links[i].hasAttribute("class")) {
+      links[i].classList.remove("active");
+    }
+  }
+  event.classList.add("active");
+
+  const category = document.querySelector(`.work__design__${id}`);
+  const categories = document.querySelectorAll(".work__design");
+  for (i = 0; i < categories.length; i++) {
+    categories[i].classList.remove("showCategory");
+    categories[i].classList.add("hideCategory");
+  }
+
+  category.classList.remove("hideCategory");
+  category.classList.add("showCategory");
+};
