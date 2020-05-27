26e00836b5671fa1387be495e0b7876e928575a1
hbjORbj <sldisek783@gmail.com>
2020-05-27 19:35:36 +0100

    Update 2020-05-27T18:35:29.540Z

--- README.md
+++
@@ -1,3 +0,0 @@
-# My (Benny Joo's) Portfolio Website
-
-Made with HTML, CSS, Vanila JS
--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const showClickedCategory = (event) => {
-  const id = event.id;
-  const links = document.querySelectorAll(".work__category button");
+var showClickedCategory = function showClickedCategory(event) {
+  var id = event.id;
+  var links = document.querySelectorAll(".work__category button");
+
   for (i = 0; i < links.length; i++) {
     if (links[i].hasAttribute("class")) {
       links[i].classList.remove("active");
     }
   }
-  event.classList.add("active");
 
-  const category = document.querySelector(`.work__design__${id}`);
-  const categories = document.querySelectorAll(".work__design");
+  event.classList.add("active");
+  var category = document.querySelector(".work__design__".concat(id));
+  var categories = document.querySelectorAll(".work__design");
+
   for (i = 0; i < categories.length; i++) {
     categories[i].classList.remove("showCategory");
     categories[i].classList.add("hideCategory");
