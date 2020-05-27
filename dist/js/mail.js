322aa3fb739e867dcee330593707047fda3728df
hbjORbj <sldisek783@gmail.com>
2020-05-27 23:48:38 +0100

    Update Contact

--- dist/js/mail.js
+++
@@ -1,9 +0,0 @@
-var submitForm = function submitForm(form) {
-  window.open(
-    "mailto:sldisek783@gmail.com?subject="
-      .concat(form.subject.value, "&body=")
-      .concat(form.message.value),
-    "_self"
-  );
-  return false;
-};
