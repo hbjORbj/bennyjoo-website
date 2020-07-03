7fead01770707e6769f0077de7f05e02468d7a2e
hbjORbj <sldisek783@gmail.com>
2020-07-03 12:28:44 +0900

    Update 2020-07-03T03:28:38.552Z

--- js/mail.js
+++ js/mail.js
@@ -1,9 +1,9 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
 var submitForm = function submitForm(form) {
-  window.open(
-    "mailto:sldisek783@gmail.com?subject="
-      .concat(form.subject.value, "&body=")
-      .concat(form.message.value),
-    "_self"
-  );
+  window.open("mailto:sldisek783@gmail.com?subject=".concat(form.subject.value, "&body=").concat(form.message.value), "_self");
   return false;
 };
+
+},{}]},{},[1]);
